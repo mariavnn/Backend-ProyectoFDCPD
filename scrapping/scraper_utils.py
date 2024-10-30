2c6a979c45c2043ccfb5e6aeb7f7081af5643a84
mariavnn <marianieto290520@gmail.com>
2024-10-29 21:36:18 -0500

    "003: Web Scrapping usando la pagina de El Pais"

--- scrapping/scraper_utils.py
+++ scrapping/scraper_utils.py
@@ -21,11 +21,11 @@ def get_news_articles():
     driver = initialize_driver()
     
     # Visita la página de noticias
-    driver.get("https://es.investing.com/news/economy")
+    driver.get("https://elpais.com/america/economia/")
     time.sleep(5)  # Espera para que la página cargue completamente
     
     # Encuentra los artículos en la página
-    articles_elements = driver.find_elements(By.CSS_SELECTOR, "a[data-test='article-title-link']")
+    articles_elements = driver.find_elements(By.CSS_SELECTOR, "h2.c_t a")
 
     # Extrae los datos de cada artículo
     news_data = []
@@ -33,12 +33,19 @@ def get_news_articles():
         try:
             title = article.text
             url = article.get_attribute("href")
-            date_element = driver.find_element(By.CSS_SELECTOR, "time[data-test='article-publish-date']")
+
+            # Extraer la fecha usando el siguiente selector CSS
+            date_element = driver.find_element(By.CSS_SELECTOR, "span.c_a_t time#sc_date")
             date = date_element.text
+            
+            # Extraer la descripción (puedes cambiar el selector si necesitas más contexto)
+            description_element = driver.find_element(By.CSS_SELECTOR, "p.c_d")
+            description = description_element.text
             
             news_data.append({
                 "title": title,
                 "date": date,
+                "description": description,
                 "url": url
             })
         except Exception as e:
